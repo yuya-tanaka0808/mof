92785216cef5d6d487699a7278c792d93471d36b
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-08-20 17:19:19 +0900

    add:物件登録ページの作成,コントローラ・モデルの設定

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,6 @@
 class Property < ApplicationRecord
-  has_many :stations
+  #アソシエーション設定,親モデルのデータを削除した時に子モデルも一緒に削除される。
+  has_many :stations, dependent: :destroy, inverse_of: :property
+  #reject_all_blank関数でtrueが出たパラメータは、送信データから除外させていく。
+  accepts_nested_attributes_for :stations, allow_destroy: true,reject_if: :all_blank
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,3 @@
 class Station < ApplicationRecord
-  belongs_to :property
+  belongs_to :property, inverse_of: :stations
 end
