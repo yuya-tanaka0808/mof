845b423e90268facf3e3838afb0de0e0b6057db4
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-08-20 15:08:18 +0900

    add:駅のmodel,アソシエーションの設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_045116) do
+ActiveRecord::Schema.define(version: 2020_08_20_052215) do
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 2020_08_20_045116) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string "station_name"
+    t.string "line_name"
+    t.integer "minutes_on_foot"
+    t.integer "property_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_stations_on_property_id"
   end
 
 end
