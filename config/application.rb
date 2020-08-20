fb37071b25ddaaa5641d6ffa38c02d652e5a4f16
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-08-20 13:46:14 +0900

    add:不要なhelper,assetsを追加しないように変更

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,9 @@ Bundler.require(*Rails.groups)
 
 module DevExam
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
