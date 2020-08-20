131d74172e334a118171234863e1b85e4cc33217
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-08-20 14:03:23 +0900

    add:物件のmodel,controller,dbテーブルの作成

---
+++ db/migrate/20200820045116_create_properties.rb
@@ -0,0 +1,12 @@
+class CreateProperties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.integer :rent
+      t.text :address
+      t.integer :age
+      t.text :content
+      t.timestamps
+    end
+  end
+end
